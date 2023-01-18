861772dde711c0db301ccf4653e7059873533f92
stevengant <sgant39@gmail.com>
2023-01-18 06:00:36 +0000

    lab07

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import './App.css';
+import '../app/App.css';
 import axios from 'axios';
-import { Card } from "react-bootstrap";
+import Weather from "../weather/Weather";
+import { Card, Container } from "react-bootstrap";
+
 
 
 class App extends React.Component {
@@ -12,7 +14,10 @@ class App extends React.Component {
       cityData: [],
       error: false,
       errorMessage: '',
-      cityName: ''
+      cityName: '',
+      lat: '',
+      lon: '',
+      dailyForecast: []
     }
   }
 
@@ -22,6 +27,29 @@ class App extends React.Component {
     })
   }
 
+  fetchWeather = async (cityName) => {
+
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/weather?searchQuery=${cityName}`;
+      // let url = await axios.get`(${process.env.REACT_APP_SERVER})/weather?lat=${cityInfo.lat}&lon=${cityInfo.lon}&searchQuery=${this.state.city}`;
+
+      let dailyForecast = await axios.get(url);
+      console.log(dailyForecast.data);
+      this.setState({
+        dailyForecast: dailyForecast.data
+      });
+
+    } catch (error) {
+      console.log(error.message);
+      console.log(error);
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      });
+
+    }
+  }
+
   getCityData = async (e) => {
     e.preventDefault();
 
@@ -29,20 +57,20 @@ class App extends React.Component {
       // DONE: need use axios to hit LocationIQ - async/await
       let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
 
-      console.log(url);
-
       let cityDataFromAxios = await axios.get(url)
-      console.log('display name', cityDataFromAxios.data[0].display_name);
-      console.log('latitude', cityDataFromAxios.data[0].lat);
-      console.log('longitude', cityDataFromAxios.data[0].lon);
+      // console.log(cityDataFromAxios.data);
 
+      // Save data to state
       this.setState({
         cityData: cityDataFromAxios.data[0],
         cityName: cityDataFromAxios.data[0].display_name,
-        latitude: cityDataFromAxios.data[0].lat,
-        longitude: cityDataFromAxios.data[0].lon,
-        error: false
-      })
+        lat: cityDataFromAxios.data[0].lat,
+        lon: cityDataFromAxios.data[0].lon,
+        error: false,
+        mapUrl: url
+      });
+      // console.log(cityDataFromAxios.data[0].display_name.split(',')[0]);
+      this.fetchWeather(cityDataFromAxios.data[0].display_name.split(',')[0]);
 
     } catch (error) {
       console.log(error);
@@ -57,17 +85,29 @@ class App extends React.Component {
     return (
       <>
         <h1>City Explorer</h1>
+        <header>
+          <form onSubmit={this.getCityData}>
+            <label htmlFor=""> Pick a City
+              <input type="text" onInput={this.handleInput} />
+              <button className='myButton' type='submit'>Explore</button>
+            </label>
+          </form>
+        </header>
+         <Container className="weatherCard">
+          {this.state.dailyForecast.map(forecast => {
+            return(
+              <Weather 
+                date={forecast.date}
+                description={forecast.description}
+              />
+            )
+          })}
 
-        <form onSubmit={this.getCityData}>
-          <label htmlFor=""> Pick a City
-            <input type="text" onInput={this.handleInput} />
-            <button className='myButton' type='submit'>Explore</button>
-          </label>
-
-        </form>
+        </Container>
 
         {/* Ternary - W ? T : F */}
         {
+
           this.state.error
             ? <Card style={{ width: '18rem' }}>
               <Card.Body>
@@ -77,11 +117,14 @@ class App extends React.Component {
             </Card>
             :
             <Card style={{ width: '18rem' }}>
-              <Card.Body>
+              {this.state.cityName && <Card.Body>
                 <h3>{this.state.cityData.display_name}</h3>
                 <h5>{this.state.cityData.lat} {this.state.cityData.lon}</h5>
-                <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=10`} alt={`Map of ${this.state.cityData.display_name}`} />
-              </Card.Body>
+                <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12`} alt={`Map of ${this.state.cityData.display_name}`} />
+                {/* {this.state.dailyForecast && this.state.dailyForecast.map(day => <p key={day.date}>{day.date}</p>)} */}
+
+              </Card.Body>}
+              
             </Card>
         }
 
